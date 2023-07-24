71225e3ad2f70572137519832f5d2fae3f933303
ivankiriiakov <ivan.kiriyakov.02@mail.ru>
2023-07-24 21:35:20 +0200

    init

--- cypress/e2e/TestFilters.cy.js
+++ cypress/e2e/TestFilters.cy.js
@@ -9,8 +9,8 @@ describe('Testing filters', () => {
         cy.get('#tracking-filter-sub-panel-platformName').click()
         cy.contains(' Google Ads ').find('button').click({force: true})
         cy.get('roi-date-picker-button').should('be.visible').click()
-        cy.get('[aria-label="2021-01-11"]').click()
-        cy.get('[aria-label="2021-01-17"]').click()
+        cy.get('[aria-label="2020-12-14"]').click()
+        cy.get('[aria-label="2020-12-20"]').click()
         cy.get('#action-buttons-ok').click()
     })
 
